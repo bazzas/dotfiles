bfdc0954d0bbe3ca18e3d38c2fb239e0f032d226
Serhii Bazarov <serhii.bazarov@gmail.com>
2019-09-08 14:52:59 +0300

    Align with my preferences

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,42 +1,12 @@
 # Vars
 	HISTFILE=~/.zsh_history
-	SAVEHIST=1000 
-	setopt inc_append_history # To save every command before it is executed 
+	SAVEHIST=1000
+	setopt inc_append_history # To save every command before it is executed
 	setopt share_history # setopt inc_append_history
-
-	git config --global push.default current
-
-# Aliases
-	alias v="vim -p"
-	mkdir -p /tmp/log
-	
-	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
-	# alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
-
 # Settings
 	export VISUAL=vim
 
 source ~/dotfiles/zsh/plugins/fixls.zsh
-
-#Functions
-	# Loop a command and show the output in vim
-	loop() {
-		echo ":cq to quit\n" > /tmp/log/output 
-		fc -ln -1 > /tmp/log/program
-		while true; do
-			cat /tmp/log/program >> /tmp/log/output ;
-			$(cat /tmp/log/program) |& tee -a /tmp/log/output ;
-			echo '\n' >> /tmp/log/output
-			vim + /tmp/log/output || break;
-			rm -rf /tmp/log/output
-		done;
-	}
-
-# Custom cd
-chpwd() ls
-
-# For vim mappings: 
-	stty -ixon
 
 # Completions
 # These are all the plugin options available: https://github.com/robbyrussell/oh-my-zsh/tree/291e96dcd034750fbe7473482508c08833b168e3/plugins
@@ -80,3 +50,11 @@ fi
 
 source ~/dotfiles/zsh/prompt.sh
 export PATH=$PATH:$HOME/dotfiles/utils
+
+# User specific aliases and functions
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias go='git checkout '
